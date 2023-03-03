266d238be94465a2cdbb4e276596ca35027d26d3
bsrcakmak <bsrckmk516@gmail.com>
2023-03-03 23:51:20 +0300

    cucumber

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -13,8 +13,8 @@ import org.junit.runner.RunWith;
         monochrome = true,//raporlarin consoleda okunakli sekilde cikmasi icin
         features = "./src/test/resources/features",//features folder path
         glue = {"stepdefinitions","hooks"},   //stepdefinitions path
-        tags = "@failed_scenario",
-        dryRun = false
+        tags = "@excel_automation",
+        dryRun = true
 )
 
         // dryRun= true da browser acilmaz sadece eksik kod var mi ona bakar,
---
+++ src/test/java/stepdefinitions/ExcelStepDefinitions.java
@@ -0,0 +1,31 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import pages.HomePage;
+import pages.LoginPage;
+import utilities.ExcelUtils;
+
+import java.util.List;
+import java.util.Map;
+
+public class ExcelStepDefinitions {
+
+    // Page objelerini olustur
+
+    HomePage blueRentalHomePage;
+    LoginPage blueRentalLoginPage;
+    ExcelUtils excelUtils;
+    List<Map<String,String>> excelDatalari;
+
+    @Given("kullanici {string} bilgileri ile giris yapar")
+    public void kullanici_bilgileri_ile_giris_yapar(String string) {
+
+        String path = "./src/test/java/resources/mysmoketestdata.xlsx";
+        String sayfa = "customer_info";
+        excelUtils = new ExcelUtils(path,sayfa);
+        excelDatalari = excelUtils.getDataList();
+
+
+    }
+
+}
